c3f96b69e0f3e821f97961be694ad53198f15ba8
ABC <abc@telekom.ru>
2008-11-18 16:59:06 +0000

    iptables 1.4.x compatibility

--- libipt_NETFLOW.c
+++ libipt_NETFLOW.c
@@ -11,8 +11,27 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iptables.h>
-//#include <linux/netfilter_ipv4/ip_tables.h>
-//#include "ipt_NETFLOW.h"
+
+#ifdef XTABLES_VERSION_CODE	// since 1.4.1
+#define MOD140
+#define iptables_target         xtables_target
+#endif
+#ifdef iptables_target		// only in 1.4.0
+#define MOD140
+#endif
+
+#ifdef MOD140
+#define ipt_entry_target	xt_entry_target
+#define register_target		xtables_register_target
+#define _IPT_ENTRY		void
+#define _IPT_IP			void
+#ifndef IPT_ALIGN
+#define IPT_ALIGN		XT_ALIGN
+#endif
+#else // before 1.3.x
+#define _IPT_ENTRY struct ipt_entry
+#define _IPT_IP struct ipt_ip
+#endif
 
 static struct option opts[] = {
   {0}
@@ -23,9 +42,13 @@ static void help(void)
 	printf( "NETFLOW target\n");
 }
 
+//static int parse(int c, char **argv, int invert, unsigned int *flags,
+//      const _IPT_ENTRY *entry,
+//      struct ipt_entry_target **target)
 static int parse(int c, char **argv, int invert, unsigned int *flags,
-      const struct ipt_entry *entry,
-      struct ipt_entry_target **target)
+	     const _IPT_ENTRY  *entry,
+	     struct ipt_entry_target **targetinfo)
+
 {
 
 	return 1;
@@ -35,11 +58,11 @@ static void final_check(unsigned int flags)
 {
 }
 
-static void save(const struct ipt_ip *ip, const struct ipt_entry_target *match)
+static void save(const _IPT_IP *ip, const struct ipt_entry_target *match)
 {
 }
 
-static void print(const struct ipt_ip *ip,
+static void print(const _IPT_IP *ip,
       const struct ipt_entry_target *target,
       int numeric)
 {
