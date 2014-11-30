1d3a962de8a1c01059e7ab0e0e0637334cfe8015
ABC <abc@telekom.ru>
2014-11-30 23:57:44 +0400

    Align license version with the Linux kernel.

--- snmp_NETFLOW.c
+++ snmp_NETFLOW.c
@@ -4,7 +4,9 @@
  *
  * (c) 2014 <abc@telekom.ru>
  *
- * License: GPL.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
 
 #include <sys/types.h>
