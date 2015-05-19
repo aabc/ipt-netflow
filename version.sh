71113f80c2e552450fe752b4346e917b2f8ea0a9
ABC <abc@telekom.ru>
2015-05-19 20:02:33 +0400

    Fixes (#38) version.sh for git-less DKMS install.
    Thanks to ktwombley@github for the report and proposed patch.

--- version.sh
+++ version.sh
@@ -7,25 +7,29 @@ PATH=$PATH:/usr/local/bin:/usr/bin:/bin
 MVERSION=`sed -n 's/^#define.*IPT_NETFLOW_VERSION.*"\(.*\)".*/\1/p' ipt_NETFLOW.c`
 
 # GITVERSION overrides base version.
-if [ -e version.h ]; then
+if [ -e version.h ] && grep -q GITVERSION version.h; then
   MVERSION=`sed -n 's/#define GITVERSION "\(.*\)".*/\1/p' version.h`
 fi
 
 # git describe overrides version from the source.
 if [ -d .git ] && which git >/dev/null 2>&1; then \
-  GVERSION=`git describe --dirty`
-  MVERSION=${GVERSION#v}
+  GVERSION=`git describe --dirty 2>/dev/null`
+  if [ "$GVERSION" ]; then
+    MVERSION=${GVERSION#v}
+  fi
 else
   GVERSION=
 fi
 
 if [ "$1" = --define ]; then
-  # output version.h which is GITVERSION or empty.
+  # called from Makefile to create version.h
+  # which should contain GITVERSION or be empty.
   if [ "$GVERSION" ]; then
     echo "#define GITVERSION \"$MVERSION\""
   else
-    echo "/* kernel doesn't like empty files */"
+    echo "/* placeholder, because kernel doesn't like empty files */"
   fi
 else
+  # normal run
   echo $MVERSION
 fi
