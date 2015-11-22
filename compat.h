aabc/ipt-netflow

commit 93160e54ef6a40c8daf5c07b0bb4be99ffb70857

Promisc mode compilation compatibility with linux 4.1.
Issue #46.

compat.h
@@ -588,4 +588,11 @@ out:
 # define vlan_tx_tag_get skb_vlan_tag_get
 # define vlan_tx_tag_present skb_vlan_tag_present
 #endif
+
+#ifndef NF_HOOK
+# define NF_HOOK_COMPAT NF_HOOK
+#else
+# define NF_HOOK_COMPAT(a,b,c,d,e,f,g) NF_HOOK(a,b,d,e,f,g)
+#endif
+
 #endif /* COMPAT_NETFLOW_H */

ipt_NETFLOW.c
@@ -1173,7 +1173,11 @@ static struct file_operations flows_seq_fops = {
 #endif /* CONFIG_PROC_FS */
 
 #ifdef ENABLE_PROMISC
-static int promisc_finish(struct sk_buff *skb)
+static int promisc_finish(
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,1,0)
+    struct sock *sk,
+#endif
+    struct sk_buff *skb)
 {
 	/* don't pass to the routing */
 	kfree_skb(skb);
@@ -1208,7 +1212,8 @@ static int promisc4_rcv(struct sk_buff *skb, struct net_device *dev, struct pack
 	memset(IPCB(skb), 0, sizeof(struct inet_skb_parm));
 	skb_orphan(skb);
 
-	return NF_HOOK(NFPROTO_IPV4, NF_INET_PRE_ROUTING, skb, dev, NULL, promisc_finish);
+	return NF_HOOK_COMPAT(NFPROTO_IPV4, NF_INET_PRE_ROUTING, NULL,
+	    skb, dev, NULL, promisc_finish);
 drop:
 	NETFLOW_STAT_INC(pkt_promisc_drop);
 	kfree_skb(skb);
@@ -1276,7 +1281,8 @@ static int promisc6_rcv(struct sk_buff *skb, struct net_device *dev, struct pack
 	rcu_read_unlock();
 	skb_orphan(skb);
 
-	return NF_HOOK(NFPROTO_IPV6, NF_INET_PRE_ROUTING, skb, dev, NULL, promisc_finish);
+	return NF_HOOK_COMPAT(NFPROTO_IPV6, NF_INET_PRE_ROUTING, NULL,
+	    skb, dev, NULL, promisc_finish);
 drop:
 	rcu_read_unlock();
 	NETFLOW_STAT_INC(pkt_promisc_drop);

commit 95fabbf805a3ebae51f0d5a54f4129ad7ed21a09

Compatibility with CentOS 7.
Fixes issue #48 caused by 93160e54ef6a40c8daf5c07b0bb4be99ffb70857.

compat.h
@@ -589,10 +589,11 @@ out:
 # define vlan_tx_tag_present skb_vlan_tag_present
 #endif
 
-#ifndef NF_HOOK
-# define NF_HOOK_COMPAT NF_HOOK
-#else
+/* NF_HOOK is define in old code, is function in new code and in Centos 7 */
+#if defined(NF_HOOK) || defined(RHEL_MAJOR)
 # define NF_HOOK_COMPAT(a,b,c,d,e,f,g) NF_HOOK(a,b,d,e,f,g)
+#else
+# define NF_HOOK_COMPAT NF_HOOK
 #endif
 
 #endif /* COMPAT_NETFLOW_H */