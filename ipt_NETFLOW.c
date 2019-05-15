aabc/ipt-netflow

commit 52e9ca4f12fcd65e586a4cb9f5df8005a4279636

Fix bind to network device.
Fixes #71 reported by michaeljbailey@github.

ipt_NETFLOW.c
@@ -1940,7 +1940,13 @@ static struct socket *usock_open_sock(struct ipt_netflow_sock *usock)
 		struct net_device *dev = dev_get_by_name(&init_net, usock->sdev);
 
 		if (dev) {
-			sock->sk->sk_bound_dev_if = dev->ifindex;
+			struct sock *sk = sock->sk;
+
+			/* SO_BINDTOIFINDEX */
+			sk->sk_bound_dev_if = dev->ifindex;
+			if (sk->sk_prot->rehash)
+				sk->sk_prot->rehash(sk);
+			sk_dst_reset(sk);
 			dev_put(dev);
 		} else {
 			printk(KERN_ERR "ipt_NETFLOW: error binding to device %s, errno %d\n",

commit 779c6bd316f4cc0978c7af16f9ad0245a3cbd0e7

Fix compilation for centos 6
`struct proto.rehash' for UDP sockets is introduced only
since 2.6.36.

Reported by ice0 and yura-mdj.

Fixes: 52e9ca4 ("Fix bind to network device.")

ipt_NETFLOW.c
@@ -1944,8 +1944,10 @@ static struct socket *usock_open_sock(struct ipt_netflow_sock *usock)
 
 			/* SO_BINDTOIFINDEX */
 			sk->sk_bound_dev_if = dev->ifindex;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,36)
 			if (sk->sk_prot->rehash)
 				sk->sk_prot->rehash(sk);
+#endif
 			sk_dst_reset(sk);
 			dev_put(dev);
 		} else {