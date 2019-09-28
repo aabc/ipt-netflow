aabc/ipt-netflow

commit 51bdc2b55257c98597199146a6cfe741e963c850

Use nf_bridge_info_get() instead of skb->nf_bridge
Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

compat.h
@@ -679,4 +679,12 @@ static inline int is_vlan_dev(struct net_device *dev)
 }
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
+static inline struct nf_bridge_info *
+nf_bridge_info_get(const struct sk_buff *skb)
+{
+	return skb->nf_bridge;
+}
+#endif
+
 #endif /* COMPAT_NETFLOW_H */

ipt_NETFLOW.c
@@ -50,6 +50,7 @@
 #include <net/addrconf.h>
 #include <net/dst.h>
 #include <linux/netfilter_ipv4/ip_tables.h>
+#include <linux/netfilter_bridge.h>
 #ifndef ENABLE_NAT
 # undef CONFIG_NF_NAT_NEEDED
 #endif
@@ -75,9 +76,6 @@
 #include "compat.h"
 #include "ipt_NETFLOW.h"
 #include "murmur3.h"
-#ifdef CONFIG_BRIDGE_NETFILTER
-# include <linux/netfilter_bridge.h>
-#endif
 #ifdef CONFIG_SYSCTL
 # include <linux/sysctl.h>
 #endif
@@ -4962,8 +4960,8 @@ static unsigned int netflow_target(
 	memset(&tuple, 0, sizeof(tuple));
 	tuple.l3proto = family;
 #ifdef ENABLE_PHYSDEV_OVER
-	if (skb->nf_bridge && skb->nf_bridge->physindev)
-		tuple.i_ifc = skb->nf_bridge->physindev->ifindex;
+	if (nf_bridge_info_get(skb) && nf_bridge_info_get(skb)->physindev)
+		tuple.i_ifc = nf_bridge_info_get(skb)->physindev->ifindex;
 	else /* FALLTHROUGH */
 #endif
 	tuple.i_ifc	= if_in? if_in->ifindex : -1;
@@ -5227,8 +5225,8 @@ do_protocols:
 		nf->tcp_flags = tcp_flags;
 		nf->o_ifc = if_out? if_out->ifindex : -1;
 #ifdef ENABLE_PHYSDEV_OVER
-		if (skb->nf_bridge && skb->nf_bridge->physoutdev)
-			nf->o_ifc = skb->nf_bridge->physoutdev->ifindex;
+		if (nf_bridge_info_get(skb) && nf_bridge_info_get(skb)->physoutdev)
+			nf->o_ifc = nf_bridge_info_get(skb)->physoutdev->ifindex;
 #endif
 
 #ifdef SNMP_RULES
@@ -5238,8 +5236,8 @@ do_protocols:
 #endif
 /* copy and snmp-resolve device with physdev overriding normal dev */
 #define copy_dev(out, physdev, dev) \
-		if (skb->nf_bridge && skb->nf_bridge->physdev) \
-			out = resolve_snmp(skb->nf_bridge->physdev); \
+		if (nf_bridge_info_get(skb) && nf_bridge_info_get(skb)->physdev) \
+			out = resolve_snmp(nf_bridge_info_get(skb)->physdev); \
 		else \
 			out = resolve_snmp(dev);
 #ifdef ENABLE_PHYSDEV

commit 9288e30f493d252ee85b492a894f78073e4f6d41

Don't compile physdev-override if BRIDGE_NETFILTER is disabled.
Fixes #120 "Compile error nf_bridge / nf_trace" reported by DocMAX.

Fixes: 51bdc2b ("Use nf_bridge_info_get() instead of skb->nf_bridge").

compat.h
@@ -680,11 +680,13 @@ static inline int is_vlan_dev(struct net_device *dev)
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
+# ifdef CONFIG_BRIDGE_NETFILTER
 static inline struct nf_bridge_info *
 nf_bridge_info_get(const struct sk_buff *skb)
 {
 	return skb->nf_bridge;
 }
+# endif
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)

configure
@@ -470,6 +470,7 @@ kernel_check_config() {
   kconfig CONFIG_NF_CONNTRACK_EVENTS	"natevents"
   kconfig CONFIG_IPV6			"IPv6"
   kconfig CONFIG_IP6_NF_IPTABLES	"ip6tables target"
+  kconfig CONFIG_BRIDGE_NETFILTER	"physdev override"
 }
 
 kernel_check_include() {

ipt_NETFLOW.c
@@ -86,6 +86,11 @@
 #if defined(CONFIG_NF_NAT_NEEDED) && LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)
 # include <net/netfilter/nf_conntrack_timestamp.h>
 #endif
+#ifdef ENABLE_PHYSDEV_OVER
+# ifndef CONFIG_BRIDGE_NETFILTER
+#  undef ENABLE_PHYSDEV_OVER
+# endif
+#endif
 
 #define IPT_NETFLOW_VERSION "2.4"   /* Note that if you are using git, you
 				       will see version in other format. */