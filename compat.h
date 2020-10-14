aabc/ipt-netflow

commit 46c0a5c80a377b05c743d20dac06b65e7dab8a02

Fix CentOS 7 compilation by compat test for 'nf_bridge_info_get'
CentOS 7 finally back-ported `nf_bridge_info_get' into their kernel, making
conditional by LINUX_VERSION_CODE too complicated. So, just detect it.

Error message:

  ipt_NETFLOW.c: In function ‘netflow_target’:
  ipt_NETFLOW.c:5294:3: error: implicit declaration of function ‘nf_bridge_info_get’ [-Werror=implicit-function-declaration]

Reported-by: https://github.com/yura-mdj
Fixes #147.

compat.h
@@ -695,14 +695,12 @@ static inline int is_vlan_dev(struct net_device *dev)
 }
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
-# ifdef CONFIG_BRIDGE_NETFILTER
+#ifndef HAVE_NF_BRIDGE_INFO_GET
 static inline struct nf_bridge_info *
 nf_bridge_info_get(const struct sk_buff *skb)
 {
 	return skb->nf_bridge;
 }
-# endif
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)

gen_compat_def
@@ -81,6 +81,8 @@ kbuild_test_struct timeval linux/ktime.h
 kbuild_test_struct proc_ops linux/proc_fs.h
 # No since v5.1, but present in CentOS-8's 4.18.0-227
 kbuild_test_symbol synchronize_sched linux/rcupdate.h
+# Fails on 3.10.0-957.10.1.el7.x86_64
+kbuild_test_symbol nf_bridge_info_get linux/netfilter_bridge.h
 
 echo "// End of compat_def.h"
 

commit 8fe543ccc0746c0bf6a1cd49e4c3dda920c98347

Fix compilation when CONFIG_BRIDGE_NETFILTER if undefined
No point to define `nf_bridge_info_get' when there is no nf_bridge.
Error message:

  compat.h:702:12: error: ‘const struct sk_buff’ has no member named ‘nf_bridge’; did you mean ‘nf_trace’

Fixes: 46c0a5c ("Fix CentOS 7 compilation by compat test for 'nf_bridge_info_get'")

compat.h
@@ -695,12 +695,14 @@ static inline int is_vlan_dev(struct net_device *dev)
 }
 #endif
 
-#ifndef HAVE_NF_BRIDGE_INFO_GET
+#ifdef CONFIG_BRIDGE_NETFILTER
+# ifdef HAVE_NF_BRIDGE_INFO_GET
 static inline struct nf_bridge_info *
 nf_bridge_info_get(const struct sk_buff *skb)
 {
 	return skb->nf_bridge;
 }
+# endif
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)

ipt_NETFLOW.c
@@ -86,9 +86,14 @@
 #if defined(CONFIG_NF_NAT_NEEDED) && LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)
 # include <net/netfilter/nf_conntrack_timestamp.h>
 #endif
-#ifdef ENABLE_PHYSDEV_OVER
-# ifndef CONFIG_BRIDGE_NETFILTER
+#ifndef CONFIG_BRIDGE_NETFILTER
+# ifdef ENABLE_PHYSDEV_OVER
+#  warning "Requested physdev override is not compiled."
 #  undef ENABLE_PHYSDEV_OVER
+# endif
+# ifdef ENABLE_PHYSDEV
+#  warning "Requested physdev is not compiled."
+#  undef ENABLE_PHYSDEV
 # endif
 #endif
 