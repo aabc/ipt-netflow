aabc/ipt-netflow

commit 8fe543ccc0746c0bf6a1cd49e4c3dda920c98347

Fix compilation when CONFIG_BRIDGE_NETFILTER if undefined
No point to define `nf_bridge_info_get' when there is no nf_bridge.
Error message:

  compat.h:702:12: error: ‘const struct sk_buff’ has no member named ‘nf_bridge’; did you mean ‘nf_trace’

Fixes: 46c0a5c ("Fix CentOS 7 compilation by compat test for 'nf_bridge_info_get'")

compat.h
@@ -695,12 +695,14 @@ static inline int is_vlan_dev(struct net_device *dev)
 }
 #endif
 
-#ifndef HAVE_NF_BRIDGE_INFO_GET
+#ifdef CONFIG_BRIDGE_NETFILTER
+# ifdef HAVE_NF_BRIDGE_INFO_GET
 static inline struct nf_bridge_info *
 nf_bridge_info_get(const struct sk_buff *skb)
 {
 	return skb->nf_bridge;
 }
+# endif
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)

ipt_NETFLOW.c
@@ -86,9 +86,14 @@
 #if defined(CONFIG_NF_NAT_NEEDED) && LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)
 # include <net/netfilter/nf_conntrack_timestamp.h>
 #endif
-#ifdef ENABLE_PHYSDEV_OVER
-# ifndef CONFIG_BRIDGE_NETFILTER
+#ifndef CONFIG_BRIDGE_NETFILTER
+# ifdef ENABLE_PHYSDEV_OVER
+#  warning "Requested physdev override is not compiled."
 #  undef ENABLE_PHYSDEV_OVER
+# endif
+# ifdef ENABLE_PHYSDEV
+#  warning "Requested physdev is not compiled."
+#  undef ENABLE_PHYSDEV
 # endif
 #endif
 

commit 83a20eff9a4469e43c9b30121a68cd3159c1e932

Fix compilation on --enable-physdev-override
    CC [M]  /opt/tools/ipt-netflow/ipt_NETFLOW.o
  In file included from /opt/tools/ipt-netflow/ipt_NETFLOW.c:76:
  /opt/tools/ipt-netflow/compat.h:701:1: error: redefinition of ???nf_bridge_info_get???
   nf_bridge_info_get(const struct sk_buff *skb)
   ^~~~~~~~~~~~~~~~~~
  In file included from /opt/tools/ipt-netflow/ipt_NETFLOW.c:53:
  ./include/linux/netfilter_bridge.h:28:1: note: previous definition of ???nf_bridge_info_get??? was here
   nf_bridge_info_get(const struct sk_buff *skb)
   ^~~~~~~~~~~~~~~~~~
  In file included from /opt/tools/ipt-netflow/ipt_NETFLOW.c:76:
  /opt/tools/ipt-netflow/compat.h: In function ???nf_bridge_info_get???:
  /opt/tools/ipt-netflow/compat.h:703:14: error: ???const struct sk_buff??? has no member named ???nf_bridge???; did you mean ???nf_trace????
    return skb->nf_bridge;
		^~~~~~~~~
		nf_trace

Fixes: 8fe543c ("Fix compilation when CONFIG_BRIDGE_NETFILTER if undefined")
Reported-by: Artem Egorkin <artem@bonzo.me>

compat.h
@@ -696,7 +696,7 @@ static inline int is_vlan_dev(struct net_device *dev)
 #endif
 
 #ifdef CONFIG_BRIDGE_NETFILTER
-# ifdef HAVE_NF_BRIDGE_INFO_GET
+# ifndef HAVE_NF_BRIDGE_INFO_GET
 static inline struct nf_bridge_info *
 nf_bridge_info_get(const struct sk_buff *skb)
 {