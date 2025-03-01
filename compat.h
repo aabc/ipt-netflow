aabc/ipt-netflow

commit 40fefb20d0d2d11a39a5cc69d08db25c95b20a16

compat: Fix __has_attribute usage
Fixes: #195
Link: https://github.com/aabc/ipt-netflow/issues/195

compat.h
@@ -774,7 +774,7 @@ struct module *find_module(const char *name)
 /* Copy from 294f69e662d1 ("compiler_attributes.h: Add 'fallthrough' pseudo
  * keyword for switch/case use") */
 #ifndef fallthrough
-# if __has_attribute(__fallthrough__)
+# if defined __has_attribute && __has_attribute(__fallthrough__)
 #  define fallthrough			__attribute__((__fallthrough__))
 # else
 #  define fallthrough			do {} while (0)  /* fallthrough */

commit 0ec2162cf645063b3fe5af6b3b3c482444a9508e

compat: Really fix __has_attribute usage
Fixes: 40fefb2 ("compat: Fix __has_attribute usage")
Signed-off-by: Andreas Beckmann <anbe@debian.org>

compat.h
@@ -773,12 +773,13 @@ struct module *find_module(const char *name)
 
 /* Copy from 294f69e662d1 ("compiler_attributes.h: Add 'fallthrough' pseudo
  * keyword for switch/case use") */
-#ifndef fallthrough
-# if defined __has_attribute && __has_attribute(__fallthrough__)
+#if !defined(fallthrough) && defined(__has_attribute)
+# if __has_attribute(__fallthrough__)
 #  define fallthrough			__attribute__((__fallthrough__))
-# else
-#  define fallthrough			do {} while (0)  /* fallthrough */
 # endif
+#endif
+#ifndef fallthrough
+#  define fallthrough			do {} while (0)  /* fallthrough */
 #endif
 
 #ifndef HAVE_NF_CT_EVENT_NOTIFIER_CT_EVENT