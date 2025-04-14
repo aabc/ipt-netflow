d91a0c7b0bdd7d82067f0632600f381bd94d92e8
ABC <abc@openwall.com>
2025-04-14 21:59:07 +0300

    test_update_config.sh: Add options such as --prepare
    Allow to configure and prepare kernel in one shot.
      --checkout= implies --defconfig.
      --prepare implies --clean.
      --kdir= / KDIR= for setting kernel source (git) tree.
      --cmd= / KBUILD_PREPARE_CMD= for applying patches, together
        with --prepare it implies --force (checkout).
      A=B options are passed to make as arguments.
    
    For example
      ./test_update_config.sh --prepare --checkout=v4.9.222 gcc-10 -no-pie -O2
    and then
      make KBUILD_CFLAGS='-no-pie -O2' CC=gcc-10

--- test_update_config.sh
+++ test_update_config.sh
@@ -4,14 +4,51 @@
 # Update default .config for values required to run tests
 #
 
-set -x
-cp -f .config .config-pkt-netflow.bak
+COLOR=11
+V() {
+	[ -t 1 ] && tput setaf "$COLOR"
+	printf '+'
+	printf ' %q' "$@"
+	printf '\n'
+	[ -t 1 ] && tput op
+	"$@"
+}
+
+for opt; do
+	case $opt in
+		--kdir=*) KDIR=${opt#*=} ;;
+		--checkout=*) BRANCH=${opt#*=} ;&
+		--defconfig) DEFCONFIG=defconfig ;;
+		--debug) ADD_DEBUG=y ;;
+		--force) FORCE=--force ;;
+		--prepare) TARGETS="prepare modules_prepare" ;&
+		--clean) CLEAN=clean ;;
+		--cmd=*) KBUILD_PREPARE_CMD=${opt#*=} ;;
+		--build) DO_BUILD=y ;;
+		--configure) DO_CONFIGURE=y ;;
+		--pick=*) PICKS+=("${opt#*=}") ;;
+		--*) echo >&2 "$0: Unknown option $opt"; exit 1 ;;
+		gcc-*) OPTS+=(CC=$opt HOSTCC=$opt) ;;
+		-no-* | -[fDWO]*) KBUILD_CFLAGS+=("$opt") ;;
+		*=*) OPTS+=("${opt%%=*}=${opt#*=}") ;;
+		*) echo >&2 "$0: Unknown argument $opt"; exit 1 ;;
+	esac
+done
+
+# Patch applying command assuming --force checkout.
+[ -n "$KBUILD_PREPARE_CMD" ] && [ -n "$TARGETS" ] && FORCE=-f
+
+[ -n "${KDIR-}" ] && V cd "$KDIR"
+[ -n "${BRANCH-}" ] && V git switch ${FORCE-} --detach "$BRANCH"
+[ -n "${DEFCONFIG-}" ] && V make $DEFCONFIG
+
+V cp -f .config .config-pkt-netflow.bak
 
 if type virtme-configkernel >/dev/null 2>&1; then
-  virtme-configkernel --update
+  V virtme-configkernel --update
 fi
 
-scripts/config \
+V scripts/config \
   -e CONFIG_VETH \
   -e CONFIG_PACKET \
   -e CONFIG_NETFILTER \
@@ -20,9 +57,11 @@ scripts/config \
   -e CONFIG_IP_NF_FILTER \
   -e CONFIG_IP_NF_IPTABLES \
   -e CONFIG_IP6_NF_FILTER \
+  -e CONFIG_NF_CONNTRACK_EVENTS \
+  -e CONFIG_VLAN_8021Q \
 
-if [ "${1-}" = debug ]; then
-scripts/config \
+if [ -n "${ADD_DEBUG-}" ]; then
+V scripts/config \
   -e CONFIG_LOCK_DEBUGGING_SUPPORT \
   -e CONFIG_PROVE_LOCKING \
   -e CONFIG_DEBUG_SPINLOCK \
@@ -31,8 +70,45 @@ scripts/config \
 
 fi
 
-make olddefconfig
+V make olddefconfig
 
-scripts/diffconfig .config-pkt-netflow.bak .config
+V scripts/diffconfig .config-pkt-netflow.bak .config || :
+
+[ -n "${CLEAN-}" ] && V make $CLEAN
+[ -n "${KBUILD_PREPARE_CMD-}" ] && V eval "$KBUILD_PREPARE_CMD" || :
+for commit in "${PICKS[@]}"; do
+	V git cherry-pick --no-commit "$commit" || :
+done
+[ -n "${KBUILD_CFLAGS-}" ] && OPTS+=("KBUILD_CFLAGS=${KBUILD_CFLAGS[*]}")
+[ -n "${TARGETS-}" ] && V make $TARGETS "${OPTS[@]}"
+
+echo
+V cd "$OLDPWD"
+COLOR=14
+
+if [ -n "${DO_CONFIGURE-}" ]; then
+	readarray -t confs <<-EOF
+	  --disable-snmp-agent
+	  --enable-aggregation
+	  --enable-natevents
+	  --enable-snmp-rules
+	  --enable-macaddress
+	  --enable-vlan
+	  --promisc-mpls
+	  --enable-direction
+	  --enable-sampler
+	  --enable-sampler=hash
+	  --enable-promisc --promisc-mpls
+	  --enable-physdev
+	  --enable-physdev-override
+	EOF
+	mv -f Makefile Makefile- || :
+	V ./configure ${confs[@]}
+fi
+
+if [ -n "${DO_BUILD-}" ]; then
+	V make "${OPTS[@]}" $CLEAN ipt_NETFLOW.ko
+	size ipt_NETFLOW.ko
+fi
 
 # export XTABLES_LIBDIR=
