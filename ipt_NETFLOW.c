aabc/ipt-netflow

commit 122662f5c19864e3340cc44e703ae24b75a26ca7

Fix preemptive enabled warning
Issue 193, we can use unstable reading because we actually don't
care much of actual CPU that executed worker

Link: https://github.com/aabc/ipt-netflow/issues/193
Link: https://github.com/aabc/ipt-netflow/pull/197

ipt_NETFLOW.c
@@ -4501,7 +4501,7 @@ static void netflow_work_fn(struct work_struct *dummy)
 	wk_count = 0;
 	wk_trylock = 0;
 	wk_llist = 0;
-	wk_cpu = smp_processor_id();
+	wk_cpu = __smp_processor_id();
 	wk_start = jiffies;
 
 	pdus = netflow_scan_and_export(DONT_FLUSH);

commit d46ef265f2084eb92daa5d59f9a11df85c809357

Fix __smp_processor_id call for older kernels
v4.2 triggered this:

  ipt_NETFLOW.c: In function ‘netflow_work_fn’:
  ipt_NETFLOW.c:4506:18: error: implicit declaration of function ‘__smp_processor_id’; did you mean ‘smp_processor_id’? [-Wimplicit-function-declaration]
   4506 |         wk_cpu = __smp_processor_id();
	|                  ^~~~~~~~~~~~~~~~~~
	|                  smp_processor_id

Fixes: 122662f ("Fix preemptive enabled warning")

ipt_NETFLOW.c
@@ -4503,7 +4503,11 @@ static void netflow_work_fn(struct work_struct *dummy)
 	wk_count = 0;
 	wk_trylock = 0;
 	wk_llist = 0;
+#ifdef __smp_processor_id
 	wk_cpu = __smp_processor_id();
+#else
+	wk_cpu = smp_processor_id();
+#endif
 	wk_start = jiffies;
 
 	pdus = netflow_scan_and_export(DONT_FLUSH);